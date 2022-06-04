d15588a6b70f211869ec72e42b3dd96233ff38cf
Mukul Kumar <101646861+MukulKumar56@users.noreply.github.com>
2022-06-04 05:35:20 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,8 @@
+# ******************TABLE  in  python************
+table_of = int(input('Enter number: '))
+table_at = int(input('Table at : '))
+
+for num in range(0, table_at+1):
+    print(f'{table_of} x {num} => {table_of*num}')
+print()
+    
